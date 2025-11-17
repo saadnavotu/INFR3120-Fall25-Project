c1fb478d11d6f47be1e9cd1643d0a0da53e301ff
Rashadk <rashad.khan1@ontariotechu.net>
2025-11-16 19:58:32 -0500

    done.

--- server/config/app.js
+++ server/config/app.js
@@ -47,7 +47,6 @@ var bookingsRouter = require('../routes/bookings');
 
 app.use('/bookings', bookingsRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
