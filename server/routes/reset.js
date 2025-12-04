695996b167590f276b04727434157f54aba0d1df
Rashadk <rashad.khan1@ontariotechu.net>
2025-12-03 23:51:30 -0500

    changes madee to reset.js

--- server/routes/reset.js
+++ server/routes/reset.js
@@ -33,4 +33,40 @@ router.post('/forgot', async (req, res) => {
 });
 
 
+router.post('/reset/:token', async (req, res) => {
+  try {
+    const { password } = req.body;
+    if (!password) return res.status(400).json({ message: 'Create new password' });
 
+    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+    const user = await User.findOne({
+      resetPasswordToken: hashedToken,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) return res.status(400).json({ message: 'Token invalid.' });
+
+    // passport -local -mongosse handles the ahsing
+
+
+    user.setPassword(password, async (err) => {
+      if (err) {
+        console.error('setPassword error', err);
+        return res.status(500).json({ message: 'Error setting password' });
+      }
+
+      // clear reset fields
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpires = undefined;
+
+      await user.save();
+      return res.json({ message: 'Password reset successful. You can now log in.' });
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
